ab41f48f987e3efc68a4347d2177a4f8967a53cf
MohamedEmara93 <mohammedomara28@gmail.com>
2022-04-25 13:42:04 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
+/////////////////////////////// NAVBAR /////////////////////////////
+let videoOffset = $('#video').offset().top;
+$(window).scroll(function () {
+    let wScroll = $(window).scrollTop();
+    if (wScroll > videoOffset - 200) {
+        $('#nav').addClass('position-fixed')
+        $('#nav').css('backgroundColor', '#fff')
+        $('#nav').css('box-shadow', '0 -13px 20px 0 #306a6c')
+        $('#nav').css('transition', 'all .3s ease')
+    } else {
+        $('#nav').removeClass('position-fixed')
+        $('#nav').css('backgroundColor', '#fff')
+    }
+})
 /////////////////////////////// HEADER SECTION /////////////////////////////
 $('.carousel').owlCarousel({
     loop: true,
     nav: true,
+    navText: [
+        "<i class='fa fa-angle-right'></i>",
+        "<i class='fa fa-angle-left'></i>"
+    ],
     rtl: true,
-    // autoplay: true,
-    // autoplayTimeout: 2000,
-    // autoplayHoverPause: true,
-    // margin: 32,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
     responsive: {
         0: {
             items: 1,
@@ -19,15 +36,41 @@ $('.carousel').owlCarousel({
         }
     }
 });
+/////////////////////////////// VIDEO /////////////////////////////
+let layer = document.querySelector('.layer');
+let play = document.querySelector('#video .play');
+let exit = document.querySelector('.exit');
+
+play.addEventListener('click', function () {
+    layer.classList.remove('hidden');
+    $(window).scrollTop(videoOffset);
+})
+
+const closeVideo = function () {
+    layer.classList.add('hidden');
+}
+//// Close The Video By Close icon
+exit.addEventListener('click', closeVideo);
+
+//// Close The Video By Keyboard
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !layer.classList.contains('hidden')) {
+        closeVideo();
+    }
+})
 /////////////////////////////// خدمات اضافيه /////////////////////////////
 $('.services').owlCarousel({
     loop: true,
     nav: true,
     rtl: true,
-    // autoplay: true,
-    // autoplayTimeout: 2000,
-    // autoplayHoverPause: true,
-    margin: 32,
+    navText: [
+        "<i class='fa fa-angle-right'></i>",
+        "<i class='fa fa-angle-left'></i>"
+    ],
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    margin: 30,
     responsive: {
         0: {
             items: 1,
@@ -40,6 +83,5 @@ $('.services').owlCarousel({
         }
     }
 });
-
 ///////////////////////////////////////
 
